28f438e06cd54722ed5fd40e08f3fae2aa7fe9f4
ThopstadRedner@gmail.com <thopstadredner@gmail.com>
2017-09-14 00:20:55 -0400

    wokring on game logic, but works for each column on click, working on linking the array of questions to a button click in that column

--- jep.js
+++ jep.js
@@ -1,12 +1,29 @@
 $(document).ready(function () {
- //$('button').Click(function() {
-   // $('start').hide();
-   $('#hosueof').popup({
-       transition: 'all 0.3s'
-   })
+    $("button#houseof").on("click" , function(){
+   alert('Hello');
+    })
+    
+   var questions =  ["Rynar" /*kor*/, "Kurn"/*Noggra*/, "Worf",/*Mogh*/ 'Jadzia', /* Martok */ 'ipsum'/*ipsum*/];
+
+for(i = 0; i < questions.length; ++i ) {
+    alert(questions[i])
+}
+
+
+
+
+
+        //alert('button 1');
+//var houseofQuestions =houseofQuestions()
+ //questions ["Rynar" /*kor*/, "Kurn"/*Noggra*/, "Worf",/*Mogh*/ 'Jadzia', /* Martok */ 'ipsum'/*ipsum*/];
  
- });
-//});  
+ //alert(questions);
+ //answers   ["kor", "Noggra", "Mogh", "Martok", "ipsum"]
+ //for (i = 0; i < houseofQuestions.length;); i++) {
+  //   alert('houseofQuestions[i]');
+ }
+ )
+
   
   
     // $('.game-instructions').mouseover(.show('hidden'))
@@ -15,4 +32,3 @@ $(document).ready(function () {
     //console.log("button clicked");
     //window.location.href='jep.js';
  
-
